510776d17ff9b1eba964daf3d2aa44f7965a4a76
parisubalan <parisubalantechfidelite>
2023-01-20 16:16:59 +0530

    Basic flow was integrated - 20.01.2023

--- app/src/main/java/com/parisubalan/bottom_navigation_kotlin/MainActivity.kt
+++ app/src/main/java/com/parisubalan/bottom_navigation_kotlin/MainActivity.kt
@@ -2,10 +2,65 @@ package com.parisubalan.bottom_navigation_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.parisubalan.bottom_navigation_kotlin.databinding.ActivityMainBinding
+import com.parisubalan.bottom_navigation_kotlin.fragments.ChatFragment
+import com.parisubalan.bottom_navigation_kotlin.fragments.HomeFragment
+import com.parisubalan.bottom_navigation_kotlin.fragments.ProfileFragment
+import com.parisubalan.bottom_navigation_kotlin.fragments.SettingFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initialization()
+    }
+
+    private fun initialization() {
+        replaceFragment(HomeFragment.getInstance("Home Screen"))
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment.getInstance("Home Screen"))
+                    true
+                }
+                R.id.chat -> {
+                    replaceFragment(ChatFragment.getInstance("Chat Screen"))
+                    true
+                }
+                R.id.settings->{
+                    replaceFragment(SettingFragment.getInstance("Settings Screen"))
+                    true
+                }
+                R.id.profile->{
+                    replaceFragment(ProfileFragment.getInstance("Profile Screen"))
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
+        binding.plusBtn.setOnClickListener {
+            Toast.makeText(this,"You are click plus icon",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun replaceFragment(fragmentName: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.mainContainer, fragmentName)
+        transaction.commitNow()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
     }
 }
---
+++ app/src/main/java/com/parisubalan/bottom_navigation_kotlin/fragments/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.parisubalan.bottom_navigation_kotlin.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.parisubalan.bottom_navigation_kotlin.R
+import com.parisubalan.bottom_navigation_kotlin.databinding.FragementLayBinding
+
+object HomeFragment : Fragment() {
+
+    lateinit var binding: FragementLayBinding
+
+    fun getInstance(screenName : String?): HomeFragment {
+        var fragment = HomeFragment
+        var bundle = Bundle()
+        bundle.putString("screenName", screenName)
+        fragment.arguments = bundle
+        return fragment
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val view: View = inflater.inflate(R.layout.fragement_lay, container, false)
+        binding = FragementLayBinding.bind(view)
+        initUI()
+        return view
+    }
+
+    private fun initUI() {
+        if (arguments != null) {
+            var bundle = arguments
+            binding.tvScreenName.text = bundle!!.getString("screenName")
+        }
+    }
+}
